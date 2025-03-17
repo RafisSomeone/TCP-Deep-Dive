968541a7d617750eb7a61898155522a1fa895641
Rafał Lisak <rafal.lisak-ext@aviv-group.com>
2025-03-17 20:23:41 +0100

    Tests for calculate checksum

--- tests/test_main.c
+++ tests/test_main.c
@@ -7,6 +7,10 @@ extern void test_packet_parse_non_tcp_packet(void);
 extern void test_valid_conversion(void);
 extern void test_single_byte(void);
 extern void test_large_value(void);
+extern void test_even_length(void);
+extern void test_odd_length(void);
+extern void test_single_carry(void);
+extern void test_double_carry(void);
 
 void setUp(void) {}
 void tearDown(void) {}
@@ -23,5 +27,9 @@ int main(void) {
     RUN_TEST(test_single_byte);
     RUN_TEST(test_large_value);
 
-    return UNITY_END();
+    RUN_TEST(test_even_length);
+    RUN_TEST(test_odd_length);
+    RUN_TEST(test_single_carry);
+    RUN_TEST(test_double_carry);
 }
+
--- tests/test_range_hex_to_decimal.c
+++ tests/test_range_hex_to_decimal.c
@@ -6,16 +6,16 @@
 
 void test_valid_conversion(void) {
     unsigned char buffer[] = {0x01, 0x23, 0x45, 0x67};
-    TEST_ASSERT_EQUAL_UINT32(0x2345, range_hex_to_decimal(buffer, 1, 3));
+    TEST_ASSERT_EQUAL_UINT32(9029, range_hex_to_decimal(buffer, 1, 3));
 }
 
 void test_single_byte(void) {
     unsigned char buffer[] = {0xAA};
-    TEST_ASSERT_EQUAL_UINT32(0xAA, range_hex_to_decimal(buffer, 0, 1));
+    TEST_ASSERT_EQUAL_UINT32(170, range_hex_to_decimal(buffer, 0, 1));
 }
 
 void test_large_value(void) {
     unsigned char buffer[] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
-    TEST_ASSERT_EQUAL_UINT32(0x0123456789ABCDEF, range_hex_to_decimal(buffer, 0, 8));
+    TEST_ASSERT_EQUAL_UINT32(81985529216486895, range_hex_to_decimal(buffer, 0, 8));
 }
 
